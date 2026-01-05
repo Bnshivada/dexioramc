0334c1c55678f1356bce89321e33eae2aff9fb34
Shivada <mehmeterensariot15@gmail.com>
2026-01-05 16:09:16 +0300

    Create sil.js

---
+++ komutlar/sil.js
@@ -0,0 +1,38 @@
+const { PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'sil',
+  async execute(message, client) {
+    if (!message.member.permissions.has(PermissionFlagsBits.ManageMessages)) {
+      return message.reply('❌ Bu komutu kullanmak için **Mesajları Yönet** yetkin olminus.');
+    }
+
+    const args = message.content.split(' ').slice(1);
+    const amount = parseInt(args[0]);
+
+    if (!amount || amount < 1 || amount > 300) {
+      return message.reply('En Fazla **300** Mesaj Silebilirsiniz..');
+    }
+
+    try {
+      const silinen = await message.channel.bulkDelete(amount, true);
+
+      const embed = new EmbedBuilder()
+        .setColor('Green')
+        .setDescription(`🧹 **${silinen.size} mesaj başarıyla silindi.**`)
+        .setFooter({
+          text: 'kuramamc.tkmc.net | KuramaMC'
+        });
+
+      const bilgi = await message.channel.send({ embeds: [embed] });
+
+      setTimeout(() => {
+        bilgi.delete().catch(() => {});
+      }, 3000);
+
+    } catch (err) {
+      console.error(err);
+      message.reply('❌ Mesajlar silinirken bir hata oluştu.');
+    }
+  }
+};
