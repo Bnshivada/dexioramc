c83020f234e64dd3680556a9293ddd6f6c52468b
Shivada <mehmeterensariot15@gmail.com>
2026-01-25 12:44:21 +0300

    Create ticket-embed.js

---
+++ komutlar/ticket-embed.js
@@ -0,0 +1,33 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "ticket-embed",
+  description: "Destek talebi embedini gönderir (Yetkili komutu)",
+  execute(message) {
+    if (!message.member.permissions.has("Administrator")) {
+      return message.reply("❌ Bu komutu sadece yetkililer kullanabilir.");
+    }
+    const randomColor = Math.floor(Math.random() * 16777215);
+
+    const embed = new EmbedBuilder()
+      .setTitle("🎟️ KuramaMC Destek Talebi 🎟️")
+      .setDescription(
+        "---------------------------------\n\n" +
+        "Destek Sistemine Hoşgeldin Oyuncu,\n" +
+        "Minecraft veya Discord Sunucumuz Üzerinde Bir Sorunla Karşılaştıysan " +
+        "Buradan Destek Talebi Oluşturup Yetkililerimize Ulaşabilirsin\n\n" +
+        "🔧 Teknik Destek\n" +
+        "💳 Ödeme İşlemleri\n" +
+        "🔑 Oyun İçi Hesap İşlemleri\n" +
+        "🤝 Partnerlik Anlaşmaları\n" +
+        "⁉️ Diğer"
+      )
+      .setColor(randomColor)
+      .setFooter({
+        text: "5.133.100.199 | KuramaMC LIFESTEAL",
+        iconURL: message.guild.iconURL({ dynamic: true })
+      });
+
+    message.channel.send({ embeds: [embed] });
+  }
+};
